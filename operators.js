9eafd0f3a880319ef8e3be01b3ad6ddfdebb190c
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2025-12-18 15:02:32 +0530

    new commit

---
+++ operators.js
@@ -0,0 +1,106 @@
+// unary operators
+
+let x = 1;
+console.log(++x);
+console.log(x);
+
+
+x = 1;
+console.log(x++);
+console.log(x);
+
+x = 1;
+console.log(--x);
+console.log(x);
+
+x = 1;
+console.log(x--);
+console.log(x);
+
+
+
+
+
+
+// binary operators
+
+// arithematic operators
+
+let a = 4;
+let b = 2;
+
+console.log("arithematic operators");
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+
+
+
+// comparison operators
+
+console.log("comparison operators");
+
+console.log(a==b);
+console.log(a!=b);
+console.log(a>b);
+console.log(a>=b);
+console.log(a<b);
+console.log(a<=b);
+console.log(a===b);
+console.log(a!==b);
+
+
+// assignment operator
+
+console.log("assignment operators");
+
+console.log(a=b);
+console.log(a=a+b);
+console.log(a-=b);
+console.log(a*=b);
+console.log(a/=b);
+console.log(a%=b);
+console.log(a**=b);
+
+
+
+
+
+// boolean values              // true = 1, -1, true, " ", "string", "false",
+                               // fasle = 0, -0, false,"", 
+
+
+
+// a = 19;
+// b = 18;
+// c = a && b
+
+// // logical operator
+
+// console.log("logical operators");
+
+// console.log(c);
+// console.log(a || b);
+// console.log(! a);
+
+
+
+
+
+// a = 1;
+// b = 1;
+
+
+// // bitwise operator
+
+// console.log("bitwise operators");
+
+// console.log(a & b);
+// console.log(a | b);
+// console.log(~ a);
+// console.log(a ^ b);
+
